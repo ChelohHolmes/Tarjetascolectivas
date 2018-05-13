863150de0c2b43b45cbd059b935d4e7bcbe32bfa
isakatak <38919964+isakatak@users.noreply.github.com>
2018-05-13 10:35:01 -0500

    Objeto tarjetero

---
+++ app/src/main/java/com/example/chelohholmes/tarjetas_colectivas/tarjetero.java
@@ -0,0 +1,19 @@
+package com.example.chelohholmes.tarjetas_colectivas;
+
+
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class tarjetero {
+    public String Nombre;
+    public String Edad;
+    public String Descripcion;
+    public String Imagen;
+    public int ID;
+
+
+    public tarjetero() {
+    }
+
+}
